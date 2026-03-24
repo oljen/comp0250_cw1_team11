4563bd78bf732cf91220adb3878758b9d46a9edb
OLJEN <94927686+oljen@users.noreply.github.com>
2026-03-24 23:32:42 +0000

    Refactor cw1_class.h and add pick_and_place function
    Removed unnecessary comments and added a pick and place function.

--- cw1_team_11/include/cw1_class.h
+++ cw1_team_11/include/cw1_class.h
@@ -1,13 +1,6 @@
-/* feel free to change any part of this file, or delete this file. In general,
-you can do whatever you want with this template code, including deleting it all
-and starting from scratch. The only requirment is to make sure your entire 
-solution is contained within the cw1_team_<your_team_number> package */
-
-// include guards, prevent .h file being defined multiple times (linker error)
 #ifndef CW1_CLASS_H_
 #define CW1_CLASS_H_
 
-// system includes
 #include <atomic>
 #include <cstdint>
 #include <memory>
@@ -16,6 +9,8 @@ solution is contained within the cw1_team_<your_team_number> package */
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/point.hpp>
 
 #include "cw1_world_spawner/srv/task1_service.hpp"
 #include "cw1_world_spawner/srv/task2_service.hpp"
@@ -24,13 +19,8 @@ solution is contained within the cw1_team_<your_team_number> package */
 class cw1
 {
 public:
-
-  /* ----- class member functions ----- */
-
-  // constructor
   explicit cw1(const rclcpp::Node::SharedPtr &node);
 
-  // service callbacks for tasks 1, 2, and 3
   void t1_callback(
     const std::shared_ptr<cw1_world_spawner::srv::Task1Service::Request> request,
     std::shared_ptr<cw1_world_spawner::srv::Task1Service::Response> response);
@@ -41,7 +31,8 @@ public:
     const std::shared_ptr<cw1_world_spawner::srv::Task3Service::Request> request,
     std::shared_ptr<cw1_world_spawner::srv::Task3Service::Response> response);
 
-  /* ----- class member variables ----- */
+  // Reusable pick and place function for Tasks 1 and 3
+  bool pick_and_place(const geometry_msgs::msg::Pose& obj_pose, const geometry_msgs::msg::Point& basket_loc);
 
   rclcpp::Node::SharedPtr node_;
   rclcpp::Service<cw1_world_spawner::srv::Task1Service>::SharedPtr t1_service_;
@@ -52,13 +43,11 @@ public:
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
 
-  // Sensor callback state bookkeeping for template diagnostics.
   std::atomic<int64_t> latest_joint_state_stamp_ns_{0};
   std::atomic<uint64_t> joint_state_msg_count_{0};
   std::atomic<int64_t> latest_cloud_stamp_ns_{0};
   std::atomic<uint64_t> cloud_msg_count_{0};
 
-  // Runtime parameters (compatibility scaffold with cw1_team_0).
   bool enable_cloud_viewer_ = false;
   bool move_home_on_start_ = false;
   bool use_path_constraints_ = false;
@@ -78,10 +67,10 @@ public:
   double place_offset_z_ = 0.35;
   double grasp_approach_offset_z_ = 0.015;
   double post_grasp_lift_z_ = 0.05;
-  double gripper_grasp_width_ = 0.02;   // tighter grip (was 0.03)
+  double gripper_grasp_width_ = 0.02;
   double joint_state_wait_timeout_sec_ = 2.0;
 
   std::string task2_capture_dir_ = "/tmp/cw1_task2_capture";
 };
 
-#endif // end of include guard for CW1_CLASS_H_
+#endif
